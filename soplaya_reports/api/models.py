4554cb3c565a1de93a2ea45064de2b9c29cdce76
LuigiSavino <luigi.savino@mail-bip.com>
2024-05-15 15:55:13 +0200

    Creazione modello Report

--- soplaya_reports/api/models.py
+++ soplaya_reports/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Report(models.Model):
+    date = models.DateField()
+    restaurant = models.CharField(max_length=100)
+    planned_hours = models.IntegerField()
+    actual_hours = models.IntegerField()
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    sells = models.DecimalField(max_digits=10, decimal_places=2)
