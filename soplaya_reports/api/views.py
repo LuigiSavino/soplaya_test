990c4f2996fecf394da15a9aed8d79e8aea6cedc
LuigiSavino <luigi.savino@mail-bip.com>
2024-05-15 16:42:55 +0200

    Aggiunta filtri

--- soplaya_reports/api/views.py
+++ soplaya_reports/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Report
 from .serializers import ReportSerializer
 
@@ -6,3 +7,6 @@ from .serializers import ReportSerializer
 class ReportViewSet(viewsets.ModelViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['restaurant', 'date']
+    ordering_fields = ['date', 'planned_hours', 'actual_hours', 'budget', 'sells']
